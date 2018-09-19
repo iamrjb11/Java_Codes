3e4b18804ea31eaf42d3dd833f000c5075a91a38
iamrjb11 <iamrjb11@gmail.com>
2018-09-19 13:37:09 +0600

    Add Pelindrome Code

---
+++ JavaApplication/src/Problem_Solve/Pelindrome.java
@@ -0,0 +1,12 @@
+package Problem_Solve;
+import java.util.*;
+
+public class Pelindrome {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter any integer number : ");
+        int num=input.nextInt();
+        
+        
+    }
+}
