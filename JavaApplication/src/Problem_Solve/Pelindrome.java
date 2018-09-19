2f8a0bf5da4087bc7c175b9c04bb4feccda73688
iamrjb11 <iamrjb11@gmail.com>
2018-09-19 14:58:09 +0600

    Modified It

--- JavaApplication/src/Problem_Solve/Pelindrome.java
+++ JavaApplication/src/Problem_Solve/Pelindrome.java
@@ -5,12 +5,23 @@ public class Pelindrome {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.print("Enter any integer number : ");
-        int num,save_num;
+        int num,save_num,result,remainder,sum=0;
         num=input.nextInt();
+        save_num=num;
         
         while(true){
-            
+            remainder=num%10;
+            sum=sum*10+remainder;
+            result=num/10;
+            if(result==0) break;
+            num=result;
         }
+        System.out.println("Input number\tProcessed number");
+        System.out.println(save_num+"  <-->  "+sum+"\n");
+        if(save_num==sum) System.out.println("YES, Pellindrome number");
+        else System.out.println("Not a Pellindrome number");
+        
+        System.out.println("\n-------------------------\n");
         
         
     }
