4b348ab4afb3313342d8d35f13c300f48ef7343d
iamrjb11 <iamrjb11@gmail.com>
2018-09-28 12:06:15 +0600

    Add Single linkage algorithm for pattern

---
+++ JavaApplication/src/Pattern_Lab/Single_Linkage_Algorithm.java
@@ -0,0 +1,22 @@
+package Pattern_Lab;
+import java.util.*;
+import java.io.File;
+import java.io.FileNotFoundException;
+public class Single_Linkage_Algorithm {
+    public static void main(String[] args) throws FileNotFoundException {
+        Scanner input = new Scanner(System.in);
+        File file = new File("D:/Java/NetBeansProjects/JavaApplication/src/Pattern_Lab/clustering.txt");
+        Scanner inputFile = new Scanner(file);
+        
+        ArrayList<Double> arA=new ArrayList<>();
+        ArrayList<Double> arB=new ArrayList<>();
+        int numOfsample;
+        numOfsample = Integer.parseInt(inputFile.next());
+        for(int i=0;i<numOfsample;i++){
+            
+        }
+        
+        
+        
+    }
+}
